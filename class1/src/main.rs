2125b11ad50645e98805b706100de2206f46b1b6
DaiTran <daitq.cntt@gmail.com>
2022-06-13 11:09:05 +0700

    done exercise 2

--- class1/src/main.rs
+++ class1/src/main.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 // Exercise 1: check whether an array is subarray of another array
 fn is_sub_array(_a: &mut [u64], _b: &mut [u64]) -> bool {
     let m = _a.len();
@@ -23,8 +25,18 @@ fn is_sub_array(_a: &mut [u64], _b: &mut [u64]) -> bool {
     return false
 }
 
+
+// Exercise 2: count words in text
+fn count_word_in_text(_s: &str, _w: &str) -> usize {
+    let re = Regex::new(format!(r#"(?i){}"#, _w).as_str()).unwrap();
+    let result = re.find_iter(_s).count();
+    return result;
+}
+
 fn main() {
-    // Exercise 1 experiment
+    // Exercise 1 tests
+    println!("---- EXERCISE 1 RESULT ----");
+
     let mut a:[u64;3] = [1, 2, 3];
     let mut b:[u64;2] = [1, 2];
     
@@ -35,6 +47,15 @@ fn main() {
     let mut d:[u64;2] = [1, 4];
     
     let r2 = is_sub_array(&mut c, &mut d);
-    println!("should be false, got {}", r2)
+    println!("should be false, got {}", r2);
 
+    println!("---------------------------");
+
+    // Exercise 2 tests
+    println!("\n---- EXERCISE 2 RESULT ----");
+
+    let s = "This is foo and FOO foo as well as FoO.";
+    let w = "foo";
+    let n = count_word_in_text(s, w);
+    println!("{} appears {} time(s) in the given string.", w, n);
 }
